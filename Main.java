8540bd9656917eba7cd1e4d55de7961dd8fdb982
Himanshu Sharma <84366162+voidknown04@users.noreply.github.com>
2023-04-24 01:18:21 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,32 @@
-import javax.swing.*;
 import java.awt.*;
+import javax.swing.*;
 
-public class Main extends JWindows {
-    
-    public void Main(String[] args){
-        setSize(700,700);
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        int x = (screenSize.width-getSize().width)/2;
-        int y = (screenSize.height-getSize().height)/2;
-        setLocation(x,y);
-        setVisible(true);
-    }
+public class Main extends JWindow {
+   Image splashScreen;
+   ImageIcon imageIcon;
+   public Main() {  
+      splashScreen = Toolkit.getDefaultToolkit().getImage("splash2.png");
+      imageIcon = new ImageIcon(splashScreen);
+      setSize(700,700);
+      Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+      int x = (screenSize.width-getSize().width)/2;
+      int y = (screenSize.height-getSize().height)/2;
+      setLocation(x,y);
+      setVisible(true);
+   }
+   public void paint(Graphics g) {
+      super.paint(g);
+      g.drawImage(splashScreen, 0, 0, this);
+   }
+   public static void main(String[]args) {
+      
+      Main splash = new Main();
+      try {
+         Thread.sleep(5000);
+         splash.dispose();
+      } catch(Exception e) {
+         e.printStackTrace();
+      }
+	  new Frame();
+       }
 }
-  
